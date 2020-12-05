09aec26faea1c5be303a818ee10023d578b1bb18
Szymon Gackowski <szymon.gackowski@gmail.com>
2020-12-05 14:06:29 +0100

    Day 4 part 2

--- app/Days/Day4/Passport.php
+++ app/Days/Day4/Passport.php
@@ -3,6 +3,7 @@ namespace App\Days\Day4;
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 /**
  * @see https://adventofcode.com/2020/day/4
@@ -81,10 +82,113 @@ class Passport
         return Arr::get($this->values, $key . '.0', null);
     }
 
-    public function validate()
+    public function validate() : bool
     {
         return $this->values
                 ->only(static::REQUIRED_FIELDS)
                 ->count() === count(static::REQUIRED_FIELDS);
+    }
+
+    public function validateDeep() : bool
+    {
+        foreach (static::REQUIRED_FIELDS as $field) {
+            if (!call_user_func([$this, 'validate' . ucfirst($field)])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected function validateByr() : bool
+    {
+        return $this->inRange($this->get('byr'), 1920, 2002);
+    }
+
+    protected function validateIyr() : bool
+    {
+        return $this->inRange($this->get('iyr'), 2010, 2020);
+    }
+
+    protected function validateEyr() : bool
+    {
+        return $this->inRange($this->get('eyr'), 2020, 2030);
+    }
+
+    protected function validateHgt() : bool
+    {
+        $hgt = $this->get('hgt');
+
+        if ($hgt === null) {
+            return false;
+        }
+
+        if (Str::endsWith($hgt, 'cm')) {
+            return $this->inRange(Str::replaceLast('cm', '', $hgt), 150, 193);
+        }
+
+        if (Str::endsWith($hgt, 'in')) {
+            return $this->inRange(Str::replaceLast('in', '', $hgt), 59, 76);
+        }
+
+        return false;
+    }
+
+    protected function validateHcl() : bool
+    {
+        $hcl = $this->get('hcl');
+
+        if ($hcl === null) {
+            return false;
+        }
+
+        if (strlen($hcl) !== 7) {
+            return false;
+        }
+
+        return !!preg_match('/\#[a-f0-9]{6}/i', $hcl);
+    }
+
+    protected function validateEcl() : bool
+    {
+        $ecl = $this->get('ecl');
+
+        if ($ecl === null) {
+            return false;
+        }
+
+        return in_array($ecl, [
+            'amb',
+            'blu',
+            'brn',
+            'gry',
+            'grn',
+            'hzl',
+            'oth',
+        ]);
+    }
+
+    protected function validatePid() : bool
+    {
+        $pid = $this->get('pid');
+
+        if ($pid === null) {
+            return false;
+        }
+
+        if (strlen($pid) !== 9) {
+            return false;
+        }
+
+        return !!preg_match('/[0-9]{9}/i', $pid);
+    }
+
+    protected function inRange($value, int $min, int $max) : bool
+    {
+        if ($value === null) {
+            return false;
+        }
+
+        return (int)$value >= $min && (int)$value <= $max;
     }
 }
--- tests/Feature/Day04Test.php
+++ tests/Feature/Day04Test.php
@@ -53,6 +53,77 @@ iyr:2011 ecl:brn hgt:59in";
         $this->assertEquals(228, $valid);
     }
 
+    /** @test */
+    public function it_passes_deep_test_for_invalid_passports()
+    {
+        // given
+        $strings = collect(explode(PHP_EOL . PHP_EOL, static::INVALID_PASSPORTS));
+
+        foreach ($strings as $string) {
+            // when
+            $passport = new Passport($string);
+
+            // then
+            $this->assertFalse($passport->validateDeep());
+        }
+    }
+
+    /** @test */
+    public function it_passes_deep_test_for_valid_passports()
+    {
+        // given
+        $strings = collect(explode(PHP_EOL . PHP_EOL, static::VALID_PASSPORTS));
+
+        foreach ($strings as $string) {
+            // when
+            $passport = new Passport($string);
+
+            // then
+            $this->assertTrue($passport->validateDeep());
+        }
+    }
+
+    /** @test */
+    public function deep_validate_batch_file()
+    {
+        // given
+        $strings = collect(explode(PHP_EOL . PHP_EOL, static::LONG_PASSPORTS));
+
+        // when
+        $valid = $strings->filter(function ($string) {
+            return (new Passport($string))->validateDeep();
+        })->count();
+
+        // then
+        $this->assertEquals(175, $valid);
+    }
+
+    const INVALID_PASSPORTS = "eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007";
+
+    const VALID_PASSPORTS = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
 
     const LONG_PASSPORTS = "pid:827837505 byr:1976
 hgt:187cm
