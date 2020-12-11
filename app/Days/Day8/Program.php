87e9ee1cc7fa353d8155b6d12ce22a7f03b1d6b5
Szymon Gackowski <szymon.gackowski@gmail.com>
2020-12-11 20:20:56 +0100

    Day 8 part 1

---
+++ app/Days/Day8/Program.php
@@ -0,0 +1,112 @@
+<?php
+namespace App\Days\Day8;
+
+class Program
+{
+    /** @var array|string[] */
+    protected $instructions;
+
+    /** @var int */
+    protected $pointer = 0;
+
+    /** @var int */
+    protected $accumulator = 0;
+
+    public function __construct(string $instructions)
+    {
+        $instructions = explode(PHP_EOL, $instructions);
+
+        $this->instructions = array_map(function ($line) {
+            $parts = explode(' ', $line);
+
+            return [
+                'instruction' => $parts[0],
+                'operator'    => substr($parts[1], 0, 1),
+                'value'       => substr($parts[1], 1),
+            ];
+        }, $instructions);
+    }
+
+    public function run() : self
+    {
+        while (!$this->shouldTerminate()) {
+            $this->processNextInstruction();
+        }
+
+        return $this;
+    }
+
+    public function getAccumulatorValue() : int
+    {
+        return $this->accumulator;
+    }
+
+    protected function processNextInstruction()
+    {
+        $this->markAsExecuted();
+        call_user_func([$this, 'execute' . ucfirst($this->currentInstruction())]);
+    }
+
+    protected function markAsExecuted()
+    {
+        $this->instructions[$this->pointer]['executed'] = true;
+    }
+
+    protected function executeNop()
+    {
+        $this->pointer++;
+    }
+
+    protected function executeAcc()
+    {
+        $this->accumulator = $this->operate(
+            $this->accumulator,
+            $this->currentOperator(),
+            $this->currentValue()
+        );
+
+        $this->pointer++;
+    }
+
+    protected function executeJmp()
+    {
+        $this->pointer = $this->operate(
+            $this->pointer,
+            $this->currentOperator(),
+            $this->currentValue()
+        );
+    }
+
+    protected function currentInstruction() : string
+    {
+        return $this->instructions[$this->pointer]['instruction'];
+    }
+
+    protected function currentOperator() : string
+    {
+        return $this->instructions[$this->pointer]['operator'];
+    }
+
+    protected function currentValue() : string
+    {
+        return $this->instructions[$this->pointer]['value'];
+    }
+
+    protected function operate(int $input, string $operator, int $value) : int
+    {
+        if ($operator === '+') {
+            return $input + $value;
+        }
+
+        if ($operator === '-') {
+            return $input - $value;
+        }
+
+        return $input;
+    }
+
+    protected function shouldTerminate() : bool
+    {
+        return !empty($this->instructions[$this->pointer]['executed']);
+    }
+}
